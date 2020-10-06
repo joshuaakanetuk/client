5ac0a29ee16fbcc8c1e5fa4729a8eec3cc3cf727
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-10-05 21:22:41 -0400

    Added info to landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,10 +7,10 @@ class Landing extends React.Component{
         return(
             <main>
                 <div className="landing__content">
-                <p> Click on the dashboard, go to projects and submit a proposal for a new project. 
+                <p> Sign in with existing accounts or sign up with your email. New sign in's are automatically client type.</p>
+                <p> Click on the dashboard, go to projects and submit a proposal for a new project (as client) or approve projects (as admin). 
                 The goal of this application in to boilerplate a freelancer/small web agency in a box. Get proposal and track information in app. 
                 </p>
-                {/* <img alt="Web diagram" src="home.png" /> */}
                 </div>
             </main>
         );
